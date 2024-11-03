bf49bc096400379e5dcd2a46c5b3450d851fb4d6
Maks <csgo20041@gmail.com>
2024-11-03 09:59:56 +0100

    AKISO 1/2

---
+++ Sem3/MonteCarlo/cmake-build-debug/CMakeFiles/MonteCarlo.dir/main.cpp.o.ddi.d
@@ -0,0 +1,358 @@
+CMakeFiles/MonteCarlo.dir/main.cpp.o.ddi: \
+ /home/maks/Programming/Studies/Sem3/MonteCarlo/main.cpp \
+ /usr/include/stdc-predef.h /usr/include/c++/14.2.1/iostream \
+ /usr/include/c++/14.2.1/bits/requires_hosted.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/c++config.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/cpu_defines.h \
+ /usr/include/c++/14.2.1/pstl/pstl_config.h \
+ /usr/include/c++/14.2.1/ostream /usr/include/c++/14.2.1/ios \
+ /usr/include/c++/14.2.1/iosfwd /usr/include/c++/14.2.1/bits/stringfwd.h \
+ /usr/include/c++/14.2.1/bits/memoryfwd.h \
+ /usr/include/c++/14.2.1/bits/postypes.h /usr/include/c++/14.2.1/cwchar \
+ /usr/include/wchar.h /usr/include/bits/libc-header-start.h \
+ /usr/include/bits/floatn.h /usr/include/bits/floatn-common.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stddef.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stdarg.h \
+ /usr/include/bits/wchar.h /usr/include/bits/types/wint_t.h \
+ /usr/include/bits/types/mbstate_t.h \
+ /usr/include/bits/types/__mbstate_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/locale_t.h \
+ /usr/include/bits/types/__locale_t.h /usr/include/c++/14.2.1/exception \
+ /usr/include/c++/14.2.1/bits/exception.h \
+ /usr/include/c++/14.2.1/bits/version.h \
+ /usr/include/c++/14.2.1/bits/exception_ptr.h \
+ /usr/include/c++/14.2.1/bits/exception_defines.h \
+ /usr/include/c++/14.2.1/bits/cxxabi_init_exception.h \
+ /usr/include/c++/14.2.1/typeinfo \
+ /usr/include/c++/14.2.1/bits/hash_bytes.h /usr/include/c++/14.2.1/new \
+ /usr/include/c++/14.2.1/bits/move.h /usr/include/c++/14.2.1/type_traits \
+ /usr/include/c++/14.2.1/bits/nested_exception.h \
+ /usr/include/c++/14.2.1/bits/char_traits.h \
+ /usr/include/c++/14.2.1/compare /usr/include/c++/14.2.1/concepts \
+ /usr/include/c++/14.2.1/bits/stl_construct.h \
+ /usr/include/c++/14.2.1/bits/stl_iterator_base_types.h \
+ /usr/include/c++/14.2.1/bits/iterator_concepts.h \
+ /usr/include/c++/14.2.1/bits/ptr_traits.h \
+ /usr/include/c++/14.2.1/bits/ranges_cmp.h \
+ /usr/include/c++/14.2.1/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/14.2.1/bits/concept_check.h \
+ /usr/include/c++/14.2.1/debug/assertions.h \
+ /usr/include/c++/14.2.1/bits/localefwd.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/c++locale.h \
+ /usr/include/c++/14.2.1/clocale /usr/include/locale.h \
+ /usr/include/bits/locale.h /usr/include/c++/14.2.1/cctype \
+ /usr/include/ctype.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+ /usr/include/bits/endian.h /usr/include/bits/endianness.h \
+ /usr/include/c++/14.2.1/bits/ios_base.h \
+ /usr/include/c++/14.2.1/ext/atomicity.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/gthr.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h \
+ /usr/include/bits/types/time_t.h \
+ /usr/include/bits/types/struct_timespec.h /usr/include/bits/sched.h \
+ /usr/include/bits/types/struct_sched_param.h /usr/include/bits/cpu-set.h \
+ /usr/include/time.h /usr/include/bits/time.h /usr/include/bits/timex.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/struct_tm.h \
+ /usr/include/bits/types/clockid_t.h /usr/include/bits/types/timer_t.h \
+ /usr/include/bits/types/struct_itimerspec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h \
+ /usr/include/bits/atomic_wide_counter.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/bits/setjmp.h \
+ /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct___jmp_buf_tag.h \
+ /usr/include/bits/pthread_stack_min-dynamic.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/atomic_word.h \
+ /usr/include/sys/single_threaded.h \
+ /usr/include/c++/14.2.1/bits/locale_classes.h \
+ /usr/include/c++/14.2.1/string /usr/include/c++/14.2.1/bits/allocator.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/c++allocator.h \
+ /usr/include/c++/14.2.1/bits/new_allocator.h \
+ /usr/include/c++/14.2.1/bits/functexcept.h \
+ /usr/include/c++/14.2.1/bits/cpp_type_traits.h \
+ /usr/include/c++/14.2.1/bits/ostream_insert.h \
+ /usr/include/c++/14.2.1/bits/cxxabi_forced.h \
+ /usr/include/c++/14.2.1/bits/stl_iterator.h \
+ /usr/include/c++/14.2.1/ext/type_traits.h \
+ /usr/include/c++/14.2.1/bits/stl_function.h \
+ /usr/include/c++/14.2.1/backward/binders.h \
+ /usr/include/c++/14.2.1/ext/numeric_traits.h \
+ /usr/include/c++/14.2.1/bits/stl_algobase.h \
+ /usr/include/c++/14.2.1/bits/stl_pair.h \
+ /usr/include/c++/14.2.1/bits/utility.h \
+ /usr/include/c++/14.2.1/debug/debug.h \
+ /usr/include/c++/14.2.1/bits/predefined_ops.h \
+ /usr/include/c++/14.2.1/bit /usr/include/c++/14.2.1/bits/refwrap.h \
+ /usr/include/c++/14.2.1/bits/invoke.h \
+ /usr/include/c++/14.2.1/bits/range_access.h \
+ /usr/include/c++/14.2.1/initializer_list \
+ /usr/include/c++/14.2.1/bits/basic_string.h \
+ /usr/include/c++/14.2.1/ext/alloc_traits.h \
+ /usr/include/c++/14.2.1/bits/alloc_traits.h \
+ /usr/include/c++/14.2.1/string_view \
+ /usr/include/c++/14.2.1/bits/functional_hash.h \
+ /usr/include/c++/14.2.1/bits/ranges_base.h \
+ /usr/include/c++/14.2.1/bits/max_size_type.h \
+ /usr/include/c++/14.2.1/numbers \
+ /usr/include/c++/14.2.1/bits/string_view.tcc \
+ /usr/include/c++/14.2.1/ext/string_conversions.h \
+ /usr/include/c++/14.2.1/cstdlib /usr/include/stdlib.h \
+ /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
+ /usr/include/sys/types.h /usr/include/bits/stdint-intn.h \
+ /usr/include/endian.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/uintn-identity.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
+ /usr/include/alloca.h /usr/include/bits/stdlib-float.h \
+ /usr/include/c++/14.2.1/bits/std_abs.h /usr/include/c++/14.2.1/cstdio \
+ /usr/include/stdio.h /usr/include/bits/types/__fpos_t.h \
+ /usr/include/bits/types/__fpos64_t.h \
+ /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/bits/stdio_lim.h /usr/include/c++/14.2.1/cerrno \
+ /usr/include/errno.h /usr/include/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/bits/types/error_t.h \
+ /usr/include/c++/14.2.1/bits/charconv.h \
+ /usr/include/c++/14.2.1/bits/basic_string.tcc \
+ /usr/include/c++/14.2.1/bits/memory_resource.h \
+ /usr/include/c++/14.2.1/cstddef \
+ /usr/include/c++/14.2.1/bits/uses_allocator.h \
+ /usr/include/c++/14.2.1/bits/uses_allocator_args.h \
+ /usr/include/c++/14.2.1/tuple /usr/include/c++/14.2.1/bits/ranges_util.h \
+ /usr/include/c++/14.2.1/bits/locale_classes.tcc \
+ /usr/include/c++/14.2.1/system_error \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/error_constants.h \
+ /usr/include/c++/14.2.1/stdexcept /usr/include/c++/14.2.1/streambuf \
+ /usr/include/c++/14.2.1/bits/streambuf.tcc \
+ /usr/include/c++/14.2.1/bits/basic_ios.h \
+ /usr/include/c++/14.2.1/bits/locale_facets.h \
+ /usr/include/c++/14.2.1/cwctype /usr/include/wctype.h \
+ /usr/include/bits/wctype-wchar.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/ctype_base.h \
+ /usr/include/c++/14.2.1/bits/streambuf_iterator.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/ctype_inline.h \
+ /usr/include/c++/14.2.1/bits/locale_facets.tcc \
+ /usr/include/c++/14.2.1/bits/basic_ios.tcc \
+ /usr/include/c++/14.2.1/bits/ostream.tcc /usr/include/c++/14.2.1/istream \
+ /usr/include/c++/14.2.1/bits/istream.tcc /usr/include/c++/14.2.1/vector \
+ /usr/include/c++/14.2.1/bits/stl_uninitialized.h \
+ /usr/include/c++/14.2.1/bits/stl_vector.h \
+ /usr/include/c++/14.2.1/bits/stl_bvector.h \
+ /usr/include/c++/14.2.1/bits/vector.tcc \
+ /usr/include/c++/14.2.1/functional \
+ /usr/include/c++/14.2.1/bits/std_function.h \
+ /usr/include/c++/14.2.1/unordered_map \
+ /usr/include/c++/14.2.1/bits/unordered_map.h \
+ /usr/include/c++/14.2.1/bits/hashtable.h \
+ /usr/include/c++/14.2.1/bits/hashtable_policy.h \
+ /usr/include/c++/14.2.1/ext/aligned_buffer.h \
+ /usr/include/c++/14.2.1/bits/enable_special_members.h \
+ /usr/include/c++/14.2.1/bits/node_handle.h \
+ /usr/include/c++/14.2.1/bits/erase_if.h /usr/include/c++/14.2.1/array \
+ /usr/include/c++/14.2.1/cmath /usr/include/math.h \
+ /usr/include/bits/math-vector.h /usr/include/bits/libm-simd-decl-stubs.h \
+ /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+ /usr/include/bits/fp-fast.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/mathcalls-narrow.h \
+ /usr/include/bits/iscanonical.h /usr/include/c++/14.2.1/bits/specfun.h \
+ /usr/include/c++/14.2.1/limits /usr/include/c++/14.2.1/tr1/gamma.tcc \
+ /usr/include/c++/14.2.1/tr1/special_function_util.h \
+ /usr/include/c++/14.2.1/tr1/bessel_function.tcc \
+ /usr/include/c++/14.2.1/tr1/beta_function.tcc \
+ /usr/include/c++/14.2.1/tr1/ell_integral.tcc \
+ /usr/include/c++/14.2.1/tr1/exp_integral.tcc \
+ /usr/include/c++/14.2.1/tr1/hypergeometric.tcc \
+ /usr/include/c++/14.2.1/tr1/legendre_function.tcc \
+ /usr/include/c++/14.2.1/tr1/modified_bessel_func.tcc \
+ /usr/include/c++/14.2.1/tr1/poly_hermite.tcc \
+ /usr/include/c++/14.2.1/tr1/poly_laguerre.tcc \
+ /usr/include/c++/14.2.1/tr1/riemann_zeta.tcc \
+ /home/maks/Programming/Studies/Sem3/MonteCarlo/matplotlibcpp.h \
+ /usr/include/python3.12/Python.h /usr/include/python3.12/patchlevel.h \
+ /usr/include/python3.12/pyconfig.h /usr/include/python3.12/pymacconfig.h \
+ /usr/include/c++/14.2.1/stdlib.h /usr/include/string.h \
+ /usr/include/strings.h /usr/include/unistd.h \
+ /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+ /usr/include/bits/confname.h /usr/include/bits/getopt_posix.h \
+ /usr/include/bits/getopt_core.h /usr/include/bits/unistd_ext.h \
+ /usr/include/linux/close_range.h /usr/include/assert.h \
+ /usr/include/python3.12/pyport.h /usr/include/inttypes.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/bits/stdint-least.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/limits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /usr/include/bits/uio_lim.h /usr/include/c++/14.2.1/math.h \
+ /usr/include/sys/time.h /usr/include/sys/stat.h /usr/include/bits/stat.h \
+ /usr/include/bits/struct_stat.h /usr/include/bits/statx.h \
+ /usr/include/linux/stat.h /usr/include/linux/types.h \
+ /usr/include/asm/types.h /usr/include/asm-generic/types.h \
+ /usr/include/asm-generic/int-ll64.h /usr/include/asm/bitsperlong.h \
+ /usr/include/asm-generic/bitsperlong.h /usr/include/linux/posix_types.h \
+ /usr/include/linux/stddef.h /usr/include/asm/posix_types.h \
+ /usr/include/asm/posix_types_64.h /usr/include/asm-generic/posix_types.h \
+ /usr/include/bits/statx-generic.h \
+ /usr/include/bits/types/struct_statx_timestamp.h \
+ /usr/include/bits/types/struct_statx.h /usr/include/python3.12/exports.h \
+ /usr/include/python3.12/pymacro.h /usr/include/python3.12/pymath.h \
+ /usr/include/python3.12/pymem.h /usr/include/python3.12/cpython/pymem.h \
+ /usr/include/python3.12/pytypedefs.h /usr/include/python3.12/pybuffer.h \
+ /usr/include/python3.12/object.h /usr/include/python3.12/pystats.h \
+ /usr/include/python3.12/cpython/object.h \
+ /usr/include/python3.12/objimpl.h \
+ /usr/include/python3.12/cpython/objimpl.h \
+ /usr/include/python3.12/typeslots.h /usr/include/python3.12/pyhash.h \
+ /usr/include/python3.12/cpython/pydebug.h \
+ /usr/include/python3.12/bytearrayobject.h \
+ /usr/include/python3.12/cpython/bytearrayobject.h \
+ /usr/include/python3.12/bytesobject.h \
+ /usr/include/python3.12/cpython/bytesobject.h \
+ /usr/include/python3.12/unicodeobject.h \
+ /usr/include/python3.12/cpython/unicodeobject.h \
+ /usr/include/python3.12/cpython/initconfig.h \
+ /usr/include/python3.12/pystate.h \
+ /usr/include/python3.12/cpython/pystate.h \
+ /usr/include/python3.12/pyerrors.h \
+ /usr/include/python3.12/cpython/pyerrors.h \
+ /usr/include/python3.12/longobject.h \
+ /usr/include/python3.12/cpython/longobject.h \
+ /usr/include/python3.12/cpython/longintrepr.h \
+ /usr/include/python3.12/boolobject.h \
+ /usr/include/python3.12/floatobject.h \
+ /usr/include/python3.12/cpython/floatobject.h \
+ /usr/include/python3.12/complexobject.h \
+ /usr/include/python3.12/cpython/complexobject.h \
+ /usr/include/python3.12/rangeobject.h \
+ /usr/include/python3.12/memoryobject.h \
+ /usr/include/python3.12/cpython/memoryobject.h \
+ /usr/include/python3.12/tupleobject.h \
+ /usr/include/python3.12/cpython/tupleobject.h \
+ /usr/include/python3.12/listobject.h \
+ /usr/include/python3.12/cpython/listobject.h \
+ /usr/include/python3.12/dictobject.h \
+ /usr/include/python3.12/cpython/dictobject.h \
+ /usr/include/python3.12/cpython/odictobject.h \
+ /usr/include/python3.12/enumobject.h /usr/include/python3.12/setobject.h \
+ /usr/include/python3.12/cpython/setobject.h \
+ /usr/include/python3.12/methodobject.h \
+ /usr/include/python3.12/cpython/methodobject.h \
+ /usr/include/python3.12/moduleobject.h \
+ /usr/include/python3.12/cpython/funcobject.h \
+ /usr/include/python3.12/cpython/classobject.h \
+ /usr/include/python3.12/fileobject.h \
+ /usr/include/python3.12/cpython/fileobject.h \
+ /usr/include/python3.12/pycapsule.h \
+ /usr/include/python3.12/cpython/code.h /usr/include/python3.12/pyframe.h \
+ /usr/include/python3.12/cpython/pyframe.h \
+ /usr/include/python3.12/traceback.h \
+ /usr/include/python3.12/cpython/traceback.h \
+ /usr/include/python3.12/sliceobject.h \
+ /usr/include/python3.12/cpython/cellobject.h \
+ /usr/include/python3.12/iterobject.h \
+ /usr/include/python3.12/cpython/genobject.h \
+ /usr/include/python3.12/descrobject.h \
+ /usr/include/python3.12/cpython/descrobject.h \
+ /usr/include/python3.12/genericaliasobject.h \
+ /usr/include/python3.12/warnings.h \
+ /usr/include/python3.12/cpython/warnings.h \
+ /usr/include/python3.12/weakrefobject.h \
+ /usr/include/python3.12/cpython/weakrefobject.h \
+ /usr/include/python3.12/structseq.h \
+ /usr/include/python3.12/cpython/picklebufobject.h \
+ /usr/include/python3.12/cpython/pytime.h \
+ /usr/include/python3.12/codecs.h /usr/include/python3.12/pythread.h \
+ /usr/include/python3.12/cpython/pythread.h \
+ /usr/include/python3.12/cpython/context.h \
+ /usr/include/python3.12/modsupport.h \
+ /usr/include/python3.12/cpython/modsupport.h \
+ /usr/include/python3.12/compile.h \
+ /usr/include/python3.12/cpython/compile.h \
+ /usr/include/python3.12/pythonrun.h \
+ /usr/include/python3.12/cpython/pythonrun.h \
+ /usr/include/python3.12/pylifecycle.h \
+ /usr/include/python3.12/cpython/pylifecycle.h \
+ /usr/include/python3.12/ceval.h /usr/include/python3.12/cpython/ceval.h \
+ /usr/include/python3.12/sysmodule.h \
+ /usr/include/python3.12/cpython/sysmodule.h \
+ /usr/include/python3.12/osmodule.h /usr/include/python3.12/intrcheck.h \
+ /usr/include/python3.12/import.h \
+ /usr/include/python3.12/cpython/import.h \
+ /usr/include/python3.12/abstract.h \
+ /usr/include/python3.12/cpython/abstract.h \
+ /usr/include/python3.12/bltinmodule.h \
+ /usr/include/python3.12/cpython/pyctype.h \
+ /usr/include/python3.12/pystrtod.h /usr/include/python3.12/pystrcmp.h \
+ /usr/include/python3.12/fileutils.h \
+ /usr/include/python3.12/cpython/fileutils.h \
+ /usr/include/python3.12/cpython/pyfpe.h \
+ /usr/include/python3.12/tracemalloc.h /usr/include/c++/14.2.1/map \
+ /usr/include/c++/14.2.1/bits/stl_tree.h \
+ /usr/include/c++/14.2.1/bits/stl_map.h \
+ /usr/include/c++/14.2.1/bits/stl_multimap.h \
+ /usr/include/c++/14.2.1/numeric \
+ /usr/include/c++/14.2.1/bits/stl_numeric.h \
+ /usr/include/c++/14.2.1/pstl/glue_numeric_defs.h \
+ /usr/include/c++/14.2.1/pstl/execution_defs.h \
+ /usr/include/c++/14.2.1/algorithm \
+ /usr/include/c++/14.2.1/bits/stl_algo.h \
+ /usr/include/c++/14.2.1/bits/algorithmfwd.h \
+ /usr/include/c++/14.2.1/bits/stl_heap.h \
+ /usr/include/c++/14.2.1/bits/uniform_int_dist.h \
+ /usr/include/c++/14.2.1/bits/stl_tempbuf.h \
+ /usr/include/c++/14.2.1/bits/ranges_algo.h \
+ /usr/include/c++/14.2.1/bits/ranges_algobase.h \
+ /usr/include/c++/14.2.1/pstl/glue_algorithm_defs.h \
+ /usr/include/c++/14.2.1/cstdint \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/arrayobject.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/ndarrayobject.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/ndarraytypes.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/npy_common.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/numpyconfig.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/_numpyconfig.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/npy_endian.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/npy_cpu.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/utils.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/_neighborhood_iterator_imp.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/dtype_api.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/__multiarray_api.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/_public_dtype_api_table.h \
+ /usr/lib/python3.12/site-packages/numpy/_core/include/numpy/npy_2_compat.h \
+ /home/maks/Programming/Studies/Sem3/MonteCarlo/random.h \
+ /usr/include/c++/14.2.1/chrono /usr/include/c++/14.2.1/bits/chrono.h \
+ /usr/include/c++/14.2.1/ratio /usr/include/c++/14.2.1/ctime \
+ /usr/include/c++/14.2.1/bits/parse_numbers.h \
+ /usr/include/c++/14.2.1/sstream /usr/include/c++/14.2.1/bits/sstream.tcc \
+ /usr/include/c++/14.2.1/bits/shared_ptr.h \
+ /usr/include/c++/14.2.1/bits/shared_ptr_base.h \
+ /usr/include/c++/14.2.1/bits/allocated_ptr.h \
+ /usr/include/c++/14.2.1/bits/unique_ptr.h \
+ /usr/include/c++/14.2.1/ext/concurrence.h \
+ /usr/include/c++/14.2.1/bits/align.h \
+ /usr/include/c++/14.2.1/bits/chrono_io.h /usr/include/c++/14.2.1/iomanip \
+ /usr/include/c++/14.2.1/locale \
+ /usr/include/c++/14.2.1/bits/locale_facets_nonio.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/time_members.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/messages_members.h \
+ /usr/include/libintl.h /usr/include/c++/14.2.1/bits/codecvt.h \
+ /usr/include/c++/14.2.1/bits/locale_facets_nonio.tcc \
+ /usr/include/c++/14.2.1/bits/locale_conv.h \
+ /usr/include/c++/14.2.1/bits/quoted_string.h \
+ /usr/include/c++/14.2.1/format /usr/include/c++/14.2.1/charconv \
+ /usr/include/c++/14.2.1/optional /usr/include/c++/14.2.1/span \
+ /usr/include/c++/14.2.1/variant /usr/include/c++/14.2.1/bits/unicode.h \
+ /usr/include/c++/14.2.1/bits/unicode-data.h \
+ /usr/include/c++/14.2.1/random /usr/include/c++/14.2.1/bits/random.h \
+ /usr/include/c++/14.2.1/x86_64-pc-linux-gnu/bits/opt_random.h \
+ /usr/include/c++/14.2.1/bits/random.tcc
