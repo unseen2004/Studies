9edf66fa12060745314cf1e2062693cf6d01741b
Maks <csgo20041@gmail.com>
2024-12-27 23:03:13 +0100

    MPIS ex 3 1/2

--- BallsAndBins/main.cpp
+++ BallsAndBins/main.cpp
@@ -1,8 +1,18 @@
 #include "experiment.h"
+#include "experiment2.h"
 #include "file_operations.h"
-#include <iostream>
 
 int main() {
-    auto results = experiment();
-    save_results_to_file(results, "results.txt");
+    //auto results = experiment();
+    //save_results_to_file(results, "results.txt");
+
+    maximum_load_balanced_allocation(1);
+    maximum_load_balanced_allocation(2);
+
+    test_insertion_sort();
+
+    communication_with_interference(0.5);
+    communication_with_interference(0.1);
+
+
 }
